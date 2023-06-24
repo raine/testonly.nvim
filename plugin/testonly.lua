2c8750b917acf22f8c1e5dea57d8b3eabfb103de
Raine Virta <raine.virta@gmail.com>
2023-06-24 23:52:10 +0300

    remove dev reload

--- plugin/testonly.lua
+++ plugin/testonly.lua
@@ -8,8 +8,4 @@ local map = vim.keymap.set
 
 map("n", "<Plug>(testonly-toggle-it)", '<cmd>lua require("testonly").toggle("it")<cr>')
 map("n", "<Plug>(testonly-toggle-describe)", '<cmd>lua require("testonly").toggle("describe")<cr>')
-map(
-	"n",
-	"<Plug>(testonly-reset)",
-	'<cmd>lua require("plenary.reload").reload_module("testonly"); require("testonly").reset_all_exclusive()<cr>'
-)
+map("n", "<Plug>(testonly-reset)", '<cmd>require("testonly").reset_all_exclusive()<cr>')
