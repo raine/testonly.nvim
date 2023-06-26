605da536045c2469ab2b8593d6299f98ccc2f55b
Raine Virta <raine.virta@gmail.com>
2023-06-26 15:02:33 +0300

    fix <Plug>(testonly-reset) command

--- plugin/testonly.lua
+++ plugin/testonly.lua
@@ -8,4 +8,4 @@ local map = vim.keymap.set
 
 map("n", "<Plug>(testonly-toggle-it)", '<cmd>lua require("testonly").toggle("it")<cr>')
 map("n", "<Plug>(testonly-toggle-describe)", '<cmd>lua require("testonly").toggle("describe")<cr>')
-map("n", "<Plug>(testonly-reset)", '<cmd>require("testonly").reset_all_exclusive()<cr>')
+map("n", "<Plug>(testonly-reset)", '<cmd>lua require("testonly").reset_all_exclusive()<cr>')
